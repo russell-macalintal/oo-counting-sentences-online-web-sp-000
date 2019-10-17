2758120f3064c83e26803e24f1d506cf3870d60d
Russell Macalintal <russell.macalintal@gmail.com>
2019-10-17 02:16:30 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.length > 1 && self.end_with?(".")
   end
 
   def question?
-
+    self.length > 1 && self.end_with?("?")
   end
 
   def exclamation?
-
+    self.length > 1 && self.end_with?("!")
   end
 
   def count_sentences
-
+    str_arr = self.split()
+    # binding.pry
+    count = 0
+    str_arr.each do |str|
+      if str.sentence? || str.question? || str.exclamation?
+        count += 1
+      end
+    end
+    count
   end
 end
